be611d41c22f54b4da01df9bef87bd1098370956
Fekinox <williamowenfowlk@gmail.com>
2024-10-29 14:26:21 -0400

    Complete schema

---
+++ db/migration/000001_create_tables.down.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+DROP TABLE IF EXISTS filled_text_fields;
+
+DROP TABLE IF EXISTS filled_radio_fields;
+
+DROP TABLE IF EXISTS filled_checkbox_fields;
+
+DROP TABLE IF EXISTS filled_form_fields;
+
+DROP TABLE IF EXISTS filled_forms;
+
+DROP TABLE IF EXISTS tasks;
+
+DROP TABLE IF EXISTS text_fields;
+
+DROP TABLE IF EXISTS radio_fields;
+
+DROP TABLE IF EXISTS checkbox_fields;
+
+DROP TABLE IF EXISTS form_fields;
+
+DROP TABLE IF EXISTS current_form_versions;
+
+DROP TABLE IF EXISTS form_versions;
+
+DROP TABLE IF EXISTS forms;
+
+DROP TABLE IF EXISTS users;
+
+COMMIT;
--- db/migration/000001_create_tables.up.sql
+++ db/migration/000001_create_tables.up.sql
@@ -1,30 +1,33 @@
 BEGIN;
 
-DO $$ BEGIN
+  DO $$ BEGIN
     CREATE TYPE field_type AS ENUM (
-        'checkbox',
-        'radio',
-        'text'
+      'checkbox',
+      'radio',
+      'text'
     );
-EXCEPTION
+    EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
 
 -- All users (both creators and clients) in the database
 CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
-  name varchar(256) NOT NULL
-    CONSTRAINT user_name_unique UNIQUE,
-  pass_hash varchar(256) NOT NULL
+  username varchar(256) NOT NULL,
+  password varchar(256) NOT NULL,
+
+  CONSTRAINT user_name_unique UNIQUE (username)
 );
 
 -- A form created by a specific user for other users to fill out
 CREATE TABLE IF NOT EXISTS forms (
   id bigserial PRIMARY KEY,
   creator_id uuid NOT NULL,
+
   CONSTRAINT fk_creator_id
     FOREIGN KEY (creator_id)
     REFERENCES users
+    ON DELETE CASCADE
 );
 
 -- A specific version of a form
@@ -32,63 +35,78 @@ CREATE TABLE IF NOT EXISTS form_versions (
   id bigserial PRIMARY KEY,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   form_id bigint,
+
   CONSTRAINT fk_form_id
     FOREIGN KEY (form_id)
     REFERENCES forms
+    ON DELETE CASCADE
 );
 
 -- Relation identifying a form with its current version
 CREATE TABLE IF NOT EXISTS current_form_versions (
   form_id bigint,
   form_version_id bigint,
+
   CONSTRAINT form_id_unique UNIQUE (form_id),
   CONSTRAINT form_v_unique UNIQUE (form_version_id),
   CONSTRAINT fk_form_id
     FOREIGN KEY (form_id)
-    REFERENCES forms,
+    REFERENCES forms
+    ON DELETE CASCADE,
   CONSTRAINT fk_form_version_id
     FOREIGN KEY (form_version_id)
     REFERENCES form_versions
+    ON DELETE CASCADE
 );
 
 -- A field within a form
 CREATE TABLE IF NOT EXISTS form_fields (
   id bigserial PRIMARY KEY,
-  form_version_id bigint REFERENCES form_versions,
+  form_version_id bigint,
   idx bigint NOT NULL,
 
   type field_type NOT NULL,
   prompt text,
   required boolean,
 
-  CONSTRAINT unique_idx UNIQUE (form_version_id, idx)
+  CONSTRAINT unique_idx UNIQUE (form_version_id, idx),
+  CONSTRAINT fk_form_version_id
+    FOREIGN KEY (form_version_id)
+    REFERENCES form_versions
+    ON DELETE CASCADE
 );
 
 -- Checkbox field: select multiple of the given options
 CREATE TABLE IF NOT EXISTS checkbox_fields (
   ff_id bigint NOT NULL,
   options text[],
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES form_fields
+    ON DELETE CASCADE
 );
 
 -- Radio field: select one option
 CREATE TABLE IF NOT EXISTS radio_fields (
   ff_id bigint NOT NULL,
   options text[],
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES form_fields
+    ON DELETE CASCADE
 );
 
 -- Text field: either short answer (single line) or long answer (multiline)
 CREATE TABLE IF NOT EXISTS text_fields (
   ff_id bigint NOT NULL,
   paragraph boolean DEFAULT false,
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES form_fields
+    ON DELETE CASCADE
 );
 
 -- Task: Created by a client who fills out a form
@@ -97,22 +115,27 @@ CREATE TABLE IF NOT EXISTS tasks (
   client_id uuid NOT NULL,
   created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
   status integer,
+
   CONSTRAINT fk_client_id
     FOREIGN KEY (client_id)
     REFERENCES users
+    ON DELETE CASCADE
 );
 
 -- Filled Form: The form that a given client has filled out
 CREATE TABLE IF NOT EXISTS filled_forms (
   task_id bigint,
   form_version_id bigint,
+
   CONSTRAINT pk_task_fv_id PRIMARY KEY (task_id),
   CONSTRAINT fk_task_id
     FOREIGN KEY (task_id)
-    REFERENCES tasks,
+    REFERENCES tasks
+    ON DELETE CASCADE,
   CONSTRAINT fk_form_version_id
     FOREIGN KEY (form_version_id)
     REFERENCES form_versions
+    ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS filled_form_fields (
@@ -121,31 +144,42 @@ CREATE TABLE IF NOT EXISTS filled_form_fields (
   idx integer,
 
   type field_type NOT NULL,
-  filled boolean
+  filled boolean,
+
+  CONSTRAINT fk_task_id
+    FOREIGN KEY (task_id)
+    REFERENCES tasks
+    ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS filled_checkbox_fields (
   ff_id bigint PRIMARY KEY,
   selected_options text[], 
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES filled_form_fields
+    ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS filled_radio_fields (
   ff_id bigint PRIMARY KEY,
   selected_option text,
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES filled_form_fields
+    ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS filled_text_fields (
   ff_id bigint PRIMARY KEY,
   content text,
+
   CONSTRAINT fk_ff_id
     FOREIGN KEY (ff_id)
     REFERENCES filled_form_fields
+    ON DELETE CASCADE
 );
 
 COMMIT;
